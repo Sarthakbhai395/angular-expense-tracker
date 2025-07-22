b3c43c987d0304fb48e5d1e701bbc8ddcd7f29d6
Sarthakbhai395 <sarthakbhatnagar2005@gmail.com>
2025-07-22 11:22:10 +0530

    your meaningful commit message

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule,CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'],
 })
 export class App {
-  protected title = 'myangularapp2';
+  itemName: string = '';
+  category: string = 'Food';
+  amount: number | null = null;
+  expenseDate: string = '';
+  notes: string = '';
+
+  expenses: any[] = [];
+
+  addExpense() {
+    if (this.itemName && this.amount != null) {
+      this.expenses.push({
+        name: this.itemName,
+        category: this.category,
+        amount: this.amount,
+        date: this.expenseDate,
+        notes: this.notes
+      });
+      this.itemName = '';
+      this.category = 'Food';
+      this.amount = null;
+      this.expenseDate = '';
+      this.notes = '';
+    }
+  }
+
+  getTotal() {
+    return this.expenses.reduce((total, expense) => total + expense.amount, 0);
+  }
+
+  getCategoryTotal(category: string): number {
+    return this.expenses
+      .filter(exp => exp.category === category)
+      .reduce((total, exp) => total + exp.amount, 0);
+  }
 }
